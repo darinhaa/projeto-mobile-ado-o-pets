54083ff4dcd61e75d6f54d891c50f734758be1b5
jordanacunha <jordana.cunha@escola.pr.gov.br>
2025-09-12 11:53:47 -0300

    pip descrição

---
+++ projeto-mobile-ado-o-pets/app/(tabs)/jor.tsx
@@ -0,0 +1,97 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/hello-wave';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Pip | 3 meses</ThemedText>
+        
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+       <br /> <ThemedText type="subtitle">Curitiba, Batel</ThemedText>
+        <ThemedText>
+          <ThemedText type="defaultSemiBold"></ThemedText> 
+          {' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({
+              ios: 'cmd + d',
+              android: 'cmd + m',
+              web: '',
+            })}
+          </ThemedText>{' '}
+
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <Link href="/modal">
+          <Link.Trigger>
+            <ThemedText type="subtitle">28/07/2025</ThemedText>
+          </Link.Trigger>
+          <Link.Preview />
+          <Link.Menu>
+            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
+            <Link.MenuAction
+              title="Share"
+              icon="square.and.arrow.up"
+              onPress={() => alert('Share pressed')}
+            />
+            <Link.Menu title="More" icon="ellipsis">
+              <Link.MenuAction
+                title="Delete"
+                icon="trash"
+                destructive
+                onPress={() => alert('Delete pressed')}
+              />
+            </Link.Menu>
+          </Link.Menu>
+        </Link>
+
+        
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">História de Pip</ThemedText>
+        <ThemedText>
+        
+          <ThemedText type="defaultSemiBold">
+Pip é um hamster sírio de 3 meses, um pequeno sonhador de personalidade tranquila e curiosa. 
+Seu maior desejo é uma família paciente que entenda sua natureza gentil, oferecendo um ambiente calmo onde possa se sentir seguro para ser ele mesmo. </ThemedText> 
+          
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
