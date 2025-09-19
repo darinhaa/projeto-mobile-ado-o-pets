caf99db2e6ed4f0f75c9fff07684f3856a43ee88
darinhaa <hiandara.wosnhaki@escola.pr.gov.br>
2025-09-19 11:20:05 -0300

    iniciando o trabalhoo

--- projeto-mobile-ado-o-pets/app/(tabs)/jor.tsx
+++ projeto-mobile-ado-o-pets/app/(tabs)/jor.tsx
@@ -7,25 +7,17 @@ import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
 
-export default function HomeScreen() 
+export default function HomeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/pip.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
         <ThemedText type="title">Pip | 3 meses</ThemedText>
         
       </ThemedView>
@@ -38,10 +30,6 @@ export default function HomeScreen()
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
               web: '',
             })}
           </ThemedText>{' '}
@@ -51,7 +39,6 @@ export default function HomeScreen()
       <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
             <ThemedText type="subtitle">28/07/2025</ThemedText>
           </Link.Trigger>
           <Link.Preview />
@@ -73,18 +60,6 @@ export default function HomeScreen()
           </Link.Menu>
         </Link>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
